ed2e3c05297f1e868ae7c10da7eafa565799dd6c
anton <anton>
2021-01-13 11:31:42 +0700

    hydrocode added

--- дживас/calc_chains.py
+++ дживас/calc_chains.py
@@ -45,6 +45,112 @@ def RY(nucl):
     else:
         return 'Y'
 
+def calc_hydro_letter(two_letters):
+    assert(len(two_letters) == 2)
+
+    if two_letters[0] == 'N' or two_letters[1] == 'N':
+        left_chain_start_code = 'N'
+        return left_chain_start_code
+
+    if two_letters == 'AA':
+        left_chain_start_code = 'k'
+    elif two_letters == 'AC':
+        left_chain_start_code = 'm'
+    elif two_letters == 'AG':
+        left_chain_start_code = 'm'
+    elif two_letters == 'AT':
+        left_chain_start_code = 'f'
+    elif two_letters == 'CA':
+        left_chain_start_code = 'k'
+    elif two_letters == 'CC':
+        left_chain_start_code = 'm'
+    elif two_letters == 'CG':
+        left_chain_start_code = 'p'
+    elif two_letters == 'CT':
+        left_chain_start_code = 'm'
+    elif two_letters == 'GA':
+        left_chain_start_code = 'k'
+    elif two_letters == 'GC':
+        left_chain_start_code = 'm'
+    elif two_letters == 'GG':
+        left_chain_start_code = 'm'
+    elif two_letters == 'GT':
+        left_chain_start_code = 'm'
+    elif two_letters == 'TA':
+        left_chain_start_code = 'p'
+    elif two_letters == 'TC':
+        left_chain_start_code = 'k'
+    elif two_letters == 'TG':
+        left_chain_start_code = 'k'
+    elif two_letters == 'TT':
+        left_chain_start_code = 'k'
+    else:
+        print(two_letters)
+        assert(False)
+
+    return left_chain_start_code
+
+def calc_hydro(seq, pos, nucl):
+    left_start = seq[pos - 1] + nucl
+    len_left = 0
+
+    left_chain_start_letter = calc_hydro_letter(left_start)
+    next_hydro_letter = left_chain_start_letter
+
+    if next_hydro_letter in {'k', 'm'}:
+        len_left = 1
+
+    seq_orig = nucl
+    seq_orig = seq[pos - 1].lower() + seq_orig
+    seq_returned = next_hydro_letter.upper()
+
+    for i in range(pos - 1, 0, -1):
+
+        next_two_letters = seq[i - 1] + seq[i]
+        next_hydro_letter = calc_hydro_letter(next_two_letters)
+
+        seq_returned = next_hydro_letter + seq_returned
+        seq_orig = seq[i - 1].lower() + seq_orig
+
+        if next_hydro_letter not in {'k', 'm'}:
+            break
+
+        len_left = len_left + 1
+
+    right_start = nucl + seq[pos + 1]
+    len_right = 0
+
+    right_chain_start_letter = calc_hydro_letter(right_start)
+    next_hydro_letter = right_chain_start_letter
+
+    if next_hydro_letter in {'k', 'm'}:
+        len_right = 1
+
+
+    seq_returned = seq_returned + next_hydro_letter.upper()
+    seq_orig =  seq_orig + seq[pos + 1].lower()
+
+    for i in range(pos + 1, len(seq)):
+        next_two_letters = seq[i] + seq[i + 1]
+        next_hydro_letter = calc_hydro_letter(next_two_letters)
+
+        seq_returned = seq_returned + next_hydro_letter
+        seq_orig =  seq_orig + seq[i + 1].lower()
+
+        if next_hydro_letter not in {'k', 'm'}:
+            break
+
+        len_right = len_right + 1
+
+    if left_chain_start_letter not in {'k', 'm'} or right_chain_start_letter not in {'k', 'm'}:
+        total_len = max(len_left, len_right)
+    else:
+        total_len = len_left + len_right
+
+    return total_len, seq_orig, seq_returned
+
+
+
 def calc_RY(seq, pos, nucl):
     R_chain_len = 0
     Y_chain_len = 0
@@ -121,7 +227,7 @@ if __name__ == '__main__':
     if ( df_snp[columns.chrom].dtype == np.int64 ) or (df_snp[columns.chrom].dtype == np.int32 ):
         df_snp[columns.chrom].dtype
 
-    df_snp = df_snp.astype({columns.chrom: str} )
+    df_snp = df_snp.astype({columns.chrom: str, columns.pos : int} )
 
     df_snp['r1_len'] = 0
     df_snp['r2_len'] = 0
@@ -132,10 +238,21 @@ if __name__ == '__main__':
     df_snp['m1'] = 0
     df_snp['m2'] = 0
 
+    df_snp['p1'] = 0
+    df_snp['p2'] = 0
+
+#    df_snp['seq1'] = ''
+#    df_snp['seq2'] = ''
+
+#    df_snp['seq1_h'] = ''
+#    df_snp['seq2_h'] = ''
+
     df_write = pd.DataFrame(columns = df_snp.columns)
 
     all_chr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11',
                '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', 'X', 'Y']
+
+    #all_chr = ['18']
 
     for cur_chr in all_chr:
 
@@ -189,6 +306,11 @@ if __name__ == '__main__':
             R1, Y1 = calc_RY(sequence, pos - 1, allele_1)
             R2, Y2 = calc_RY(sequence, pos - 1, allele_2)
 
+            km1, seq1, seq1_h = calc_hydro(sequence, pos - 1, allele_1)
+            km2, seq2, seq2_h = calc_hydro(sequence, pos - 1, allele_2)
+
+
+
             df_part_write.at[index, 'r1_len'] = R1
             df_part_write.at[index, 'y1_len'] = Y1
 
@@ -198,6 +320,16 @@ if __name__ == '__main__':
             df_part_write.at[index, 'y2_len'] = Y2
 
             df_part_write.at[index, 'm2'] = max(R2, Y2)
+
+            df_part_write.at[index, 'p1'] = km1
+            df_part_write.at[index, 'p2'] = km2
+
+    #        df_part_write.at[index, 'seq1'] = seq1
+    #        df_part_write.at[index, 'seq2'] = seq2
+
+    #        df_part_write.at[index, 'seq1_h'] = seq1_h
+    #        df_part_write.at[index, 'seq2_h'] = seq2_h
+
 
         #df_write = df_part_write[[columns.id, columns.chrom, columns.pos, columns.other, columns.effect, 'r1_len', 'y1_len', 'r2_len', 'y2_len']]
 
