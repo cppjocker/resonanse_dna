31d2d889779aff832f5720a470de87de6fff1401
anton <anton>
2021-09-29 10:27:57 +0700

    update shoulder metrics

--- дживас/seq_utils.py
+++ дживас/seq_utils.py
@@ -46,26 +46,44 @@ def calc_AT_metric(seq, pos, nucl, code):
         return calc_AT_metric2(seq, pos, nucl)
 
 
-def calc_shoulder_metric(seq, pos, nucl, code):
-    if not ( nucl == code[0] and seq[pos + 1] == code[1] ):
-        return -1, -1, -1, False
+def calc_shoulder_metric_2(seq, pos, nucl, code, break_code):
+    if not (seq[pos - 1] == code[0]  and nucl == code[1]):
+        return -1, -1, -1
 
     l1 = 0
-    for walk_left in range(pos - 2, 0, -1):
-        if (seq[walk_left] == code[0] and seq[walk_left + 1] == code[1]):
-            l1 = (pos - 1) - (walk_left + 1)
+    for walk_left in range(pos - 3, 0, -1):
+        if (seq[walk_left] == break_code[0] and seq[walk_left + 1] == break_code[1]):
+            l1 = (pos - 2) - (walk_left + 1)
             break
 
     l2 = 0
     for walk_right in range (pos + 1, len(seq) ):
-        if ( seq[walk_right] == code[0] and seq[walk_right + 1] == code[1] ):
+        if ( seq[walk_right] == break_code[0] and seq[walk_right + 1] == break_code[1] ):
+            l2 = (walk_right - 1) - (pos)
+            break
+
+    return l1, l2, l1 * l2
+
+def calc_shoulder_metric_1(seq, pos, nucl, code, break_code):
+    if not (nucl == code[0] and seq[pos + 1] == code[1]):
+        return -1, -1, -1
+
+    l1 = 0
+    for walk_left in range(pos - 2, 0, -1):
+        if (seq[walk_left] == break_code[0] and seq[walk_left + 1] == break_code[1]):
+            l1 = (pos - 1) - (walk_left + 1)
+            break
+
+    l2 = 0
+    for walk_right in range(pos + 1, len(seq)):
+        if (seq[walk_right] == break_code[0] and seq[walk_right + 1] == break_code[1]):
             l2 = (walk_right - 1) - (pos + 1)
             break
 
-    AT_in = False
+    return l1, l2, l1 * l2
 
-    if "AT" in seq[walk_left:(walk_right + 1)]:
-        AT_in = True
-
-
-    return l1, l2, l1*l2, AT_in
+def calc_shoulder_metric(seq, pos, nucl, code, break_code, case):
+    if case == 1:
+        return calc_shoulder_metric_1(seq, pos, nucl, code, break_code)
+    elif case == 2:
+        return calc_shoulder_metric_2(seq, pos, nucl, code, break_code)
